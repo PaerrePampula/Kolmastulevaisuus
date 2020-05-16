fa46ef9523cdf3f9661f55aec9c6c0384a13267d
PaerrePampula <Patrik19100@student.hamk.fi>
2020-05-16 22:48:01 +0300

    Enemmän funktionaalisempi skriptableobjektin käyttö
    Nytten eventControlliin pystyy listata erilaisia scriptableobject listoja, jossa on eri dialogeja vaikka koululle, kodille, työpaikalle jne. Tällä hetkellä siellä on ainoastaan yksi lista jota haetaan debugimetodilla, joka poistuu myöhemmin.

--- Assets/EventControl.cs
+++ Assets/EventControl.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class EventControl : MonoBehaviour
 {
+    GameObject randomEventUIBox;
+    public Transform canvas;
+    public List<RandomEventScriptable> schoolRandomEvents;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-    void CreateEventBox()
-    {
+        randomEventUIBox = Resources.Load<GameObject>("RandomEventContainer");
+        DEBUGCreateEventBox();
 
+    }
+    void DEBUGCreateEventBox() //Tämä on silkkaa debugia, spawnaa random eventin randomisti valittuna.
+    {
+        GameObject randomevent = Instantiate(randomEventUIBox);
+        randomevent.transform.SetParent(canvas);
+        randomevent.GetComponent<RandomEventUI>().setRandomEvent(schoolRandomEvents[randomizedRandomEventIndexChoice(schoolRandomEvents)]);
+        randomevent.transform.localPosition = Vector3.zero;
+    }
+    int randomizedRandomEventIndexChoice(List<RandomEventScriptable> chosenlist)
+    {
+        int index = Random.Range(0, chosenlist.Count + 1);
+        return index;
     }
     // Update is called once per frame
     void Update()
