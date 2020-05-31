244a319326f8baf00f265795976d9d12f269a6c9
PaerrePampula <Patrik19100@student.hamk.fi>
2020-05-31 14:00:52 +0300

    Uuden main menun tekeminen - ei toiminnallinen toistaiseksi
    -puolivaiheessa, toistaiseksi scene ei päästä pelaajaa eteenpäin

---
+++ Assets/RentableHome.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RentableHome
+{
+    string address;
+    float baseRentAmount;
+    float waterCost;
+    float electricityCost;
+    float homeInsurance;
+    bool homeInsuranceNeeded;
+    (string, float)[] extrasCost;
+    string longFormDescription;
+    string perks;
+    string shortFormDescription;
+    string extrasDescription;
+    GameObject prefab;
+    public RentableHome(string rentAddress, float baseRent, string shortDescription, GameObject rentHomePrefab , float rentWater = 0,
+        float rentElectricity = 0, float rentInsurance = 0, bool doesNeedToHaveInsurance = false,
+        (string,float)[] extrasRentCost = null, string rentLongFormDescription = "", string Rentperks = "", 
+        string RentextrasDescription = "")
+    {
+        address = rentAddress;
+        baseRentAmount = baseRent;
+        shortFormDescription = shortDescription;
+        prefab = rentHomePrefab;
+        waterCost = rentWater;
+        electricityCost = rentElectricity;
+        homeInsurance = rentInsurance;
+        homeInsuranceNeeded = doesNeedToHaveInsurance;
+        extrasCost = extrasRentCost;
+        longFormDescription = rentLongFormDescription;
+        perks = Rentperks;
+        extrasDescription = RentextrasDescription;
+    }
+}
---
+++ Assets/Scripts/ScriptableObject/RentableHomeScriptable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RentableHomeScriptable : MonoBehaviour
+{
+    public string address; 
+    public float baseRentAmount;
+    public float waterCost;
+
+    public float electricityCost; //Simuloidaan sitten sähkönsiirto jonakin randomina arvona.
+    //Tehdään myös oletus että sähkö kuuluu vuokraan.
+
+    public float homeInsurance;
+    public bool needToHaveHomeInsurance;
+    public (string, float)[] extrasCost; //esim saunan hinta, autoparkin hinta.
+
+    public string longFormDescription = "esim. uusi kolmio lähellä" +
+        "keskustaa! Välittömässä läheisyydessä paljon kauppoja," +
+        "sekä paikalliset korkeakoulut!";
+    public string perks = "esim oma sauna, lasitettu parveke";
+    public string shortFormDescription = "esim 1h + s + p";
+    public string extrasDescription = "esim vuokraan sisältyy 25M laajakaista";
+    public GameObject prefab; //Vuokrakämpän malli.
+    //voisi myöhemmin lisätä jonkin kuvankaappausgallerian kämpistä ehkä?
+}
