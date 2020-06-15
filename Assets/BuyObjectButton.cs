f326873bd390e2cfc91484df907b6043e911f04b
PaerrePampula <Patrik19100@student.hamk.fi>
2020-06-15 10:27:38 +0300

    uusi buymenu ja muita
    + rendererimage pyöriviä buymenun objekteja varten

---
+++ Assets/BuyObjectButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class BuyObjectButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField]
+    RawImage rendImage;
+    [SerializeField]
+    GameObject buyObjectPrefab;
+    public delegate void Hover(GameObject gameObject);
+    public static event Hover OnHover;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        rendImage.gameObject.SetActive(true);
+        OnHover.Invoke(buyObjectPrefab);
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        rendImage.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/BuyObjectRendMesh.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyObjectRendMesh : MonoBehaviour
+{
+    GameObject displayObject;
+    private void OnEnable()
+    {
+        BuyObjectButton.OnHover += instantiateNewHoverObject;
+    }
+    private void OnDisable()
+    {
+        BuyObjectButton.OnHover -= instantiateNewHoverObject;
+    }
+    void instantiateNewHoverObject(GameObject newHover)
+    {
+        if (displayObject != null)
+        {
+            Destroy(displayObject);
+        }
+        displayObject = Instantiate(newHover, transform);
+        displayObject.transform.localPosition = Vector3.zero;
+        displayObject.transform.rotation = Quaternion.identity;
+
+    }
+}
