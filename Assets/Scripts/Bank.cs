f5539db429dd6a9c05e25bdfc9ce804287f1e520
PaerrePampula <Patrik19100@student.hamk.fi>
2020-06-24 11:27:29 +0300

    Säästämiseen littyviä puolivalmiita elementtejä

---
+++ Assets/Scripts/Bank.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bank : MonoBehaviour
+{
+    PlayerMoney savedMoney;
+
+    public PlayerMoney SavedMoney { get => savedMoney; set => savedMoney = value; }
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        Flag.OnFlagFire += checkFlag;
+        SavedMoney = new PlayerMoney();
+    }
+    private void OnDestroy()
+    {
+        Flag.OnFlagFire -= checkFlag;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void checkFlag(Flag flag)
+    {
+        switch (flag.FlagName)
+        {
+            case "SAVING_50":
+                addSavings(50);
+                break;
+            case "SAVING_25":
+                addSavings(25);
+                break;
+            default:
+                break;
+        }
+    }
+    void addSavings(float amount)
+    {
+        SavedMoney.MoneyChange(amount);
+    }
+}
