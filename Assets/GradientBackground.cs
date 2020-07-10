ac9fd156f77dc125ce311690cfcc9643a6de20d0
PaerrePampula <Patrik19100@student.hamk.fi>
2020-07-10 11:14:08 +0300

    Paranneltu kaupunkiscene sekä uusi taustagradient

---
+++ Assets/GradientBackground.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GradientBackground : MonoBehaviour
+{
+    public UnityEngine.UI.RawImage img;
+
+    public Color32 color1;
+
+    public Color32 color2;
+    private Texture2D backgroundTexture;
+
+    void OnEnable()
+    {
+
+        backgroundTexture = new Texture2D(1, 2);
+        backgroundTexture.wrapMode = TextureWrapMode.Clamp;
+        backgroundTexture.filterMode = FilterMode.Bilinear;
+        SetColor(color1,color2);
+        backgroundTexture.Apply();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            SetColor(color1, color2);
+        }
+    }
+
+    public void SetColor(Color32 color1, Color32 color2)
+    {
+        backgroundTexture.SetPixels32(new Color32[] { color1, color2 });
+        backgroundTexture.Apply();
+        img.texture = backgroundTexture;
+    }
+}
--- Assets/Scripts/Secret/EasterEgg.cs
+++ Assets/Scripts/Secret/EasterEgg.cs
@@ -19,10 +19,10 @@ public class EasterEgg : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            eggEnable = true;
-        }
+        //if (Input.GetKeyDown(KeyCode.F))
+        //{
+        //    eggEnable = true;
+        //}
         if (eggEnable == true && Input.GetMouseButtonDown(0))
         {
 
